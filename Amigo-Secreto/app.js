b32b6f5b59b3a3818b6e0cdb79d622823f4af7b9
Daiane3006 <daiane.correia.oliveira77@gmail.com>
2025-02-15 15:38:48 -0300

    Adicionando as funcionalidades no app.js

--- Amigo-Secreto/app.js
+++ Amigo-Secreto/app.js
@@ -1,1 +1,32 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+
+let lista = [];
+
+function adicionarAmigo() {
+
+    let amigo = document.getElementById('amigo');
+    let item = amigo.value.trim();
+
+    if (item) {
+        lista.push(item);
+        let li = document.createElement('li');
+        li.textContent = item;
+
+        document.getElementById('listaAmigos').appendChild(li);
+        amigo.value = '';
+    }else{
+        alert('Por favor, insira um nome!');
+    }
+}
+
+function sortearAmigo() {
+    if (lista.length > 0) {
+        let sorteado = lista[Math.floor(Math.random() * lista.length)];
+        document.getElementById('resultado').textContent = "O amigo secreto sorteado é: " + sorteado;
+
+        document.getElementById('listaAmigos').innerHTML = '';
+        lista[sorteado];
+    }
+
+}
